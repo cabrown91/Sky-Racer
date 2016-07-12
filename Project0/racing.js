7f4bd2f2bf0d802edc90023a4071c7aa86a4c617
Alicia Brown <cleo.aliciabrown@gmail.com>
2016-07-12 05:50:10 -0700

    Update racing.js

--- Project0/racing.js
+++ Project0/racing.js
@@ -10,17 +10,4 @@ var Player = function (name) {
 
 };
 
-var position1 = 0;
-var movePlane1 = function(e) {
-  if (e.which === 97 && position1 < 100) {
-    position1 += 10;
-    console.log(position1);
-    $('#plane1').animate({
-    left: position1 + "%"
-    }, 100);
-
-    if(position1 === 100) {
-      console.log('player1 wins');
-    }
-  }
 };
